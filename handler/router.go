70005660afe462de678ebf74682aa5f3898ab9f6
kid2Ion <hiroki.kondo@zuuonline.com>
2022-10-23 18:44:45 +0900

    implement router

---
+++ handler/router.go
@@ -0,0 +1,12 @@
+package handler
+
+import "github.com/labstack/echo"
+
+func InitRouting(e *echo.Echo, diaryHandler DiaryHandler) {
+	e.GET("/", diaryHandler.View())
+	e.GET("/search", diaryHandler.Search())
+	e.GET("/filter", diaryHandler.SearchByTag())
+	e.POST("/new", diaryHandler.Add())
+	e.PUT("/edit/:id", diaryHandler.Edit())
+	e.DELETE("/delete/:id", diaryHandler.Delete())
+}
