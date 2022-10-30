a14c6a7762e65fbdb83d14c516df12516f409a1b
kid2Ion <hiroki.kondo@zuuonline.com>
2022-10-30 17:35:54 +0900

    inject tag

--- injector/injector.go
+++ injector/injector.go
@@ -17,12 +17,27 @@ func InjectDiaryRepository() repository.DiaryRepository {
 	return infra.NewDiaryRepository(sqlHandler)
 }
 
+func InjectTagRepository() repository.TagRepository {
+	sqlHandler := InjectDB()
+	return infra.NewTagRepository(sqlHandler)
+}
+
 func InjectDiaryUsecase() usecase.DiaryUsecase {
 	diaryRepository := InjectDiaryRepository()
 	return usecase.NewDiaryUsecase(diaryRepository)
 }
 
+func InjectTagUsecase() usecase.TagUsecase {
+	tagRepository := InjectTagRepository()
+	return usecase.NewTagUsecase(tagRepository)
+}
+
 func InjectDiaryHandler() handler.DiaryHandler {
 	diaryHandler := InjectDiaryUsecase()
 	return handler.NewDiaryHandler(diaryHandler)
+}
+
+func InjectTagHandler() handler.TagHandler {
+	tagUsecase := InjectTagUsecase()
+	return handler.NewTagHandler(tagUsecase)
 }
