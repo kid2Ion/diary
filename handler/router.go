e2297a41d790388e891915179ab0927213dde50f
kid2Ion <hiroki.kondo@zuuonline.com>
2022-10-30 17:36:26 +0900

    implement tag router

--- handler/router.go
+++ handler/router.go
@@ -2,11 +2,13 @@ package handler
 
 import "github.com/labstack/echo"
 
-func InitRouting(e *echo.Echo, diaryHandler DiaryHandler) {
+func InitRouting(e *echo.Echo, diaryHandler DiaryHandler, tagHandler TagHandler) {
 	e.GET("/", diaryHandler.View())
 	e.GET("/search", diaryHandler.Search())
 	e.GET("/filter", diaryHandler.SearchByTag())
 	e.POST("/new", diaryHandler.Add())
 	e.PUT("/edit/:id", diaryHandler.Edit())
 	e.DELETE("/delete/:id", diaryHandler.Delete())
+
+	e.GET("tags", tagHandler.View())
 }
