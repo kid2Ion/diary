71d12efc6672eb2cfefb2eaf53c1f7f71378cf28
kid2Ion <hiroki.kondo@zuuonline.com>
2022-11-03 15:49:47 +0900

    add midllewear

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 	"fmt"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	fmt.Println("---start server---")
+	// Middleware
 	diaryHandler := injector.InjectDiaryHandler()
 	tagHandler := injector.InjectTagHandler()
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 	handler.InitRouting(e, diaryHandler, tagHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
