8096810edb71308ac6aba071b30b8404976d4546
kid2Ion <hiroki.kondo@zuuonline.com>
2022-11-03 15:50:46 +0900

    implement tag handler

--- handler/tag.go
+++ handler/tag.go
@@ -1,14 +1,19 @@
 package handler
 
 import (
+	"diary/domain/model"
 	"diary/usecase"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
 
 type TagHandler interface {
 	View() echo.HandlerFunc
+	Add() echo.HandlerFunc
+	Edit() echo.HandlerFunc
+	Delete() echo.HandlerFunc
 }
 
 type tagHandler struct {
@@ -24,8 +29,70 @@ func (th *tagHandler) View() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		tags, err := th.TagUsecase.View()
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, err)
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
 		}
 		return c.JSON(http.StatusOK, tags)
+	}
+}
+
+func (th *tagHandler) Add() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var tag model.Tag
+		c.Bind(&tag)
+		id, err := th.TagUsecase.Add(&tag)
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
+		}
+		return c.JSON(http.StatusOK, id)
+	}
+}
+
+func (th *tagHandler) Edit() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
+		}
+		var tag model.Tag
+		c.Bind(&tag)
+		err = th.TagUsecase.Edit(id, &tag)
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
+		}
+
+		return c.JSON(http.StatusOK, nil)
+	}
+}
+
+func (th *tagHandler) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
+		}
+		err = th.TagUsecase.Delete(id)
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: err.Error(),
+			}
+		}
+
+		return c.JSON(http.StatusOK, nil)
 	}
 }
