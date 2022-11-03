3f0f11794b8d3d6e80affa34aab5ba2d29320ffe
kid2Ion <hiroki.kondo@zuuonline.com>
2022-11-03 15:51:29 +0900

    implement tag repository

--- domain/repository/tag.go
+++ domain/repository/tag.go
@@ -4,7 +4,7 @@ import "diary/domain/model"
 
 type TagRepository interface {
 	FindAll() ([]*model.Tag, error)
-	// Create(tag *model.Tag) (int64, error)
-	// Update(id int, tag *model.Tag) error
-	// Delete(id int) error
+	Create(tag *model.Tag) (int64, error)
+	Update(id int, tag *model.Tag) error
+	Delete(id int) error
 }
