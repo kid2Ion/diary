937910b48aa6b2c125c5f85ca0f3ea92aa1c4ada
kid2Ion <hiroki.kondo@zuuonline.com>
2022-11-03 17:32:38 +0900

    implement vue

---
+++ resources/src/components/Diary.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/三大将.png" width="80%">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'DiaryIndex',
+});
+</script>
